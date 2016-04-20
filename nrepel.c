c453d50802731341a1a59885ec33570051b312e9
Luciano <lucianodato@gmail.com>
2016-04-20 19:27:11 -0300

    Misc

--- nrepel.c
+++ nrepel.c
@@ -1,7 +1,7 @@
 /*
     noise-repellent -- Noise Reduction LV2
     
-    Copyright 2016 Luciano Dato <ldato@gmail.com>
+    Copyright 2016 Luciano Dato <lucianodato@gmail.com>
     
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
