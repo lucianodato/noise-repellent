00b9c17e58bcf42f5117aba70828b89990615ae3
Luciano <lucianodato@gmail.com>
2018-03-29 10:47:37 -0300

    Comment the installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+#remove previous build
 rm -rf build || true
+#remove previous install
 sudo rm -rf /usr/local/lib/lv2/nrepel.lv2 || true
+
+#make a new build directory
 mkdir -p build
+
+#build the plugin in the new directory
 cd build
 meson .. --buildtype release --strip
 ninja -j2 -v
+
+#install the plugin in the system
 sudo ninja install
 
 
