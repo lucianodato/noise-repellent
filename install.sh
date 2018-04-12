22441ea4d69d6a6d4f79c18a44f2878ef6178a5f
Luciano Dato <lucianodato@gmail.com>
2018-04-12 17:38:57 -0300

    Only remove the build directory previously

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 #remove previous build
 rm -rf build || true
-#remove previous install
-sudo rm -rf /usr/local/lib/lv2/nrepellent.lv2 || true
 
 #build the plugin in the new directory
 meson build --buildtype release
