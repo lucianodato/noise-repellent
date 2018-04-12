02c6150f5429b3a4dc914728d4223ec1c560a5b2
Luciano Dato <lucianodato@gmail.com>
2018-04-12 17:37:53 -0300

    Install script should only contemplate the build directory removal

--- install.sh
+++ install.sh
@@ -2,8 +2,6 @@
 
 #remove previous build
 rm -rf build || true
-#remove previous install
-sudo rm -rf /usr/local/lib/lv2/nrepel.lv2 || true
 
 #build the plugin in the new directory
 meson build --buildtype release
