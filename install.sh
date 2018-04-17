30b78751d91b0a1ad848faec3e38ccba3f2fc61f
Luciano Dato <lucianodato@gmail.com>
2018-04-16 22:23:50 -0300

    Avoid os specific actions in install script

--- install.sh
+++ install.sh
@@ -2,8 +2,8 @@
 
 #remove previous build
 rm -rf build || true
-sudo rm -rf /Library/Audio/Plug-Ins/LV2/nrepel.lv2 || true
-
+#sudo rm -rf /Library/Audio/Plug-Ins/LV2/nrepel.lv2 || true
+#sudo rm -rf /usr/local/lib/lv2/nrepel.lv2 || true
 
 #build the plugin in the new directory
 meson build --buildtype release
