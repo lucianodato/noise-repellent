0b36ba1745577e175e7cc6c6ec4f864e996487c8
Luciano Dato <lucianodato@gmail.com>
2018-04-16 22:18:59 -0300

    Fixed bug #54. It turns out that clang needs the flag -mrecip to optimize reciprocals operations.

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 #remove previous build
 rm -rf build || true
+sudo rm -rf /Library/Audio/Plug-Ins/LV2/nrepel.lv2 || true
+
 
 #build the plugin in the new directory
 meson build --buildtype release
