f9aa4115827c19b428fdbfd08953ac91cb0ea16a
Luciano Dato <lucianodato@gmail.com>
2021-12-08 17:01:41 +0100

    Move plugin state management back to plugin.c. Fix segfault happening with plugin state. Added lv2apply to tasks. Added logging support for hosts that support it

--- src/plugin.c
+++ src/plugin.c
@@ -19,14 +19,56 @@ along with this program.  If not, see <http://www.gnu.org/licenses/
 
 #include "denoise_parameters.h"
 #include "fft_denoiser.h"
+#include "lv2/atom/atom.h"
+#include "lv2/atom/forge.h"
+#include "lv2/atom/util.h"
+#include "lv2/core/lv2.h"
+#include "lv2/core/lv2_util.h"
+#include "lv2/log/log.h"
+#include "lv2/log/logger.h"
+#include "lv2/state/state.h"
+#include "lv2/urid/urid.h"
 #include "noise_profile.h"
-#include "plugin_state.h"
 #include "stft_processor.h"
+
 #include <stdlib.h>
 #include <string.h>
 
+#define NOISEREPELLENT_URI "https://github.com/lucianodato/noise-repellent"
+#define N_PROPS 2
+
 #define FFT_SIZE 2048
 #define OVERLAP_FACTOR 4
+
+typedef struct
+{
+	LV2_URID atom_Int;
+	LV2_URID atom_Vector;
+	LV2_URID plugin;
+	LV2_URID atom_URID;
+} URIs;
+
+typedef struct
+{
+	LV2_URID property_fft_size;
+	LV2_URID property_saved_noise_profile;
+} State;
+
+static inline void
+map_uris(LV2_URID_Map *map, URIs *uris)
+{
+	uris->plugin = map->map(map->handle, NOISEREPELLENT_URI);
+	uris->atom_Int = map->map(map->handle, LV2_ATOM__Int);
+	uris->atom_Vector = map->map(map->handle, LV2_ATOM__Vector);
+	uris->atom_URID = map->map(map->handle, LV2_ATOM__URID);
+}
+
+static inline void
+map_state(LV2_URID_Map *map, State *state)
+{
+	state->property_fft_size = map->map(map->handle, NOISEREPELLENT_URI "#fftsize");
+	state->property_saved_noise_profile = map->map(map->handle, NOISEREPELLENT_URI "#savednoiseprofile");
+}
 
 typedef enum
 {
@@ -61,7 +103,11 @@ typedef struct
 	float *transient_protection;
 	float *enable;
 
-	PluginState *plugin_state;
+	// Features
+	LV2_URID_Map *map;
+	LV2_Log_Logger log;
+	URIs uris;
+	State state;
 
 	DenoiseParameters *denoise_parameters;
 	NoiseProfile *noise_profile;
@@ -72,24 +118,32 @@ typedef struct
 static LV2_Handle instantiate(const LV2_Descriptor *descriptor, double rate, const char *bundle_path,
 							  const LV2_Feature *const *features)
 {
-
 	NoiseRepellent *self = (NoiseRepellent *)calloc(1, sizeof(NoiseRepellent));
 
-	self->plugin_state = plugin_state_initialize();
+	const char *missing = lv2_features_query(
+		features,
+		LV2_LOG__log, &self->log.log, false,
+		LV2_URID__map, &self->map, true,
+		NULL);
 
-	if (!plugin_state_configure(self->plugin_state, features))
+	lv2_log_logger_set_map(&self->log, self->map);
+
+	if (missing)
 	{
-		plugin_state_free(self->plugin_state);
+		lv2_log_error(&self->log, "Missing feature <%s>\n", missing);
 		free(self);
-		return (LV2_Handle)NULL;
+		return NULL;
 	}
+
+	map_uris(self->map, &self->uris);
+	map_state(self->map, &self->state);
+
+	self->sample_rate = (float)rate;
 
 	self->noise_profile = noise_profile_initialize(FFT_SIZE / 2 + 1);
 	self->denoise_parameters = plugin_parameters_initialize();
 	self->fft_denoiser = fft_denoiser_initialize(self->sample_rate, FFT_SIZE, OVERLAP_FACTOR);
 	self->stft_processor = stft_processor_initialize(self->fft_denoiser, FFT_SIZE, OVERLAP_FACTOR);
-
-	self->sample_rate = (float)rate;
 
 	return (LV2_Handle)self;
 }
@@ -168,7 +222,6 @@ static void cleanup(LV2_Handle instance)
 	plugin_parameters_free(self->denoise_parameters);
 	fft_denoiser_free(self->fft_denoiser);
 	stft_processor_free(self->stft_processor);
-	plugin_state_free(self->plugin_state);
 	free(instance);
 }
 
@@ -177,8 +230,14 @@ static LV2_State_Status savestate(LV2_Handle instance, LV2_State_Store_Function 
 {
 	NoiseRepellent *self = (NoiseRepellent *)instance;
 
-	plugin_state_savestate(self->plugin_state, store, handle, FFT_SIZE,
-						   self->noise_profile);
+	const int fft_size = FFT_SIZE;
+	const float *noise_profile = get_noise_profile(self->noise_profile);
+
+	store(handle, self->state.property_fft_size, &fft_size, sizeof(int), self->uris.atom_Int,
+		  LV2_STATE_IS_POD | LV2_STATE_IS_PORTABLE);
+
+	store(handle, self->state.property_saved_noise_profile, (void *)noise_profile, sizeof(float) * fft_size / 2 + 1,
+		  self->uris.atom_Vector, LV2_STATE_IS_POD | LV2_STATE_IS_PORTABLE);
 
 	return LV2_STATE_SUCCESS;
 }
@@ -189,17 +248,28 @@ static LV2_State_Status restorestate(LV2_Handle instance, LV2_State_Retrieve_Fun
 {
 	NoiseRepellent *self = (NoiseRepellent *)instance;
 
-	int fft_size;
-	if (!plugin_state_restorestate(self->plugin_state, retrieve, handle,
-								   self->noise_profile, &fft_size))
-	{
-		if (!fft_size)
-			set_spectral_size(self->stft_processor, FFT_SIZE);
-		else
-			set_spectral_size(self->stft_processor, fft_size);
+	size_t size;
+	uint32_t type;
+	uint32_t valflags;
 
+	const int *fftsize = (const int *)retrieve(handle, self->state.property_fft_size, &size, &type, &valflags);
+	if (!fftsize || type != self->uris.atom_Int)
+	{
 		return LV2_STATE_ERR_NO_PROPERTY;
 	}
+
+	if (!*fftsize)
+		set_spectral_size(self->stft_processor, FFT_SIZE);
+	else
+		set_spectral_size(self->stft_processor, *fftsize);
+
+	const void *saved_noise_profile = retrieve(handle, self->state.property_saved_noise_profile, &size, &type, &valflags);
+	if (!saved_noise_profile || size != sizeof(float) * *fftsize / 2 + 1 || type != self->uris.atom_Vector)
+	{
+		return LV2_STATE_ERR_NO_PROPERTY;
+	}
+
+	set_noise_profile(self->noise_profile, (float *)LV2_ATOM_BODY(saved_noise_profile));
 
 	return LV2_STATE_SUCCESS;
 }
