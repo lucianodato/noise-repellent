3a4f870955c9b1426740ebdeed0a35412666dbf9
Luciano Dato <lucianodato@gmail.com>
2022-04-18 23:47:18 +0200

    Update license headers

--- src/noise_profile_state.c
+++ src/noise_profile_state.c
@@ -1,20 +1,21 @@
 /*
 noise-repellent -- Noise Reduction LV2
 
-Copyright 2021 Luciano Dato <lucianodato@gmail.com>
+Copyright 2022 Luciano Dato <lucianodato@gmail.com>
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Lesser General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 3 of the License, or (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Lesser General Public License for more details.
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
 
 You should have received a copy of the GNU Lesser General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 #include "noise_profile_state.h"
